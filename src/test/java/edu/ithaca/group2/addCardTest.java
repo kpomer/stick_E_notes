4aa27b4af5ec4f6eb33d34a7dd164db2091560f1
Kevin Pomer <kpomer@ithaca.edu>
2018-03-25 15:36:10 -0400

    Priority test passes, others do not

--- src/test/java/edu/ithaca/group2/addCardTest.java
+++ src/test/java/edu/ithaca/group2/addCardTest.java
@@ -1,15 +1,14 @@
 package edu.ithaca.group2;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-
-
 public class addCardTest {
     //Cards contain the fields String title, String description, and int priority (which decides the order of the list)
 
-    Workspace testWorkspace = new Workspace();
-
-    @org.junit.jupiter.api.Test
-    public void addNewCardTests() {
+    @Test
+    public void fieldsCorrectTest(){
+        Workspace testWorkspace = new Workspace();
 
         Card testCard1 = new Card("testCard1Title", 0, "testCard1Description", "1/1/2020");
         Card testCard2 = new Card("testCard2Title", 1);
@@ -35,18 +34,29 @@ public class addCardTest {
         assertEquals("", testWorkspace.getCard(2).getDescription());
         assertEquals(2, testWorkspace.getCard(2).getPriority());
         assertEquals("", testWorkspace.getCard(2).getDeadline());
+    }
+
+    @Test
+    public void priorityNumberTests() {
+        Workspace testWorkspace = new Workspace();
 
         //If there are n cards within the workspace, and a priority of <0 or >(n+1) is entered when creating a new card, print error message
-        Card testCard4 = new Card("testCard4Title",5, "testCard4Description", "1/1/2020");
-        Card testCard5 = new Card("testCard5Title", -1, "testCard5Description",  "1/1/2020");
+        Card testCard4 = new Card("testCard4Title", 5, "testCard4Description", "1/1/2020");
+        Card testCard5 = new Card("testCard5Title", -1, "testCard5Description", "1/1/2020");
         assertEquals(-1, testWorkspace.addCard(testCard4));
         assertEquals(-1, testWorkspace.addCard(testCard5));
 
+    }
+
+    @Test
+    public void stringTitleTest() {
+        Workspace testWorkspace = new Workspace();
+
         //If empty list is entered for String title, return -1, otherwise return 0
-        Card testCard8 = new Card("", 1, "", "1/1/2020");
-        Card testCard9 = new Card("testCard9", 1, "", "1/1/2020");
-        assertEquals(-1, testWorkspace.addCard(testCard8));
+        Card testCard8 = new Card("", 0, "", "1/1/2020");
+        Card testCard9 = new Card("testCard9", 0, "", "1/1/2020");
         assertEquals(0, testWorkspace.addCard(testCard9));
+        assertEquals(-1, testWorkspace.addCard(testCard8));
 
     }
 
