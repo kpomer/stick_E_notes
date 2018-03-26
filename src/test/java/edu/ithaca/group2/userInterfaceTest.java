ea270b948ee5a1e392e1a02723d6df87bd321919
Javaughn <javaughnmiller@gmail.com>
2018-03-25 21:15:57 -0400

    First commit to comandLine branch

---
+++ src/test/java/edu/ithaca/group2/userInterfaceTest.java
@@ -0,0 +1,21 @@
+package edu.ithaca.group2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class userInterfaceTest {
+    @Test
+    void OptionsTest(){
+        int userInput = 1;
+        userInterface test = new userInterface();
+        assertEquals(0,test.Options(userInput), "this should pass");
+    }
+
+    @Test
+    void OutOfRangeTest(){
+        int userInput = 24;
+        userInterface test = new userInterface();
+        assertEquals(-1,test.Options(userInput), "this should pass");
+    }
+}
