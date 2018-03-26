8dcd585de3d0c3264290875186b7e7592133bc3c
Kevin Pomer <kpomer@ithaca.edu>
2018-03-26 10:02:06 -0400

    made changes in viewAll to allow project to build

--- src/main/java/edu/ithaca/group2/veiwAll.java
+++ src/main/java/edu/ithaca/group2/veiwAll.java
@@ -1,14 +1,11 @@
 package edu.ithaca.group2;
 
 public class veiwAll {
-    public static void main(String[] args) {
-        System.out.println("Here are your cards");
-        for (int i = 0; i <Cardlist.size ; i++) {
-            System.out.println(Cardlist[i].iD+" ." + Cardlist[i].title);
-
-
-
-
+    public void viewAllCards() {
+        Workspace theWorkspace = new Workspace();
+        System.out.println("Here are your cards:");
+        for (int i = 0; i <theWorkspace.cardCount ; i++) {
+            System.out.println(theWorkspace.cardList.get(i) +" ." + theWorkspace.cardList.get(i));
 
         }
     }
