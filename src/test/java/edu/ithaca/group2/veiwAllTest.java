9e5f70c8f9c50cb0fd28a4663d4ef84b5ffa4652
Kevin Pomer <kpomer@ithaca.edu>
2018-04-04 09:42:40 -0400

    created tests for viewAllCards.  Tests fail

--- src/test/java/edu/ithaca/group2/veiwAllTest.java
+++ src/test/java/edu/ithaca/group2/veiwAllTest.java
@@ -1,11 +1,39 @@
 package edu.ithaca.group2;
 
-import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class veiwAllTest {
-    String a = "1. Do Laundry";
+
+    @Test
+    void testEmptyString(){
+        Workspace testWorkspace = new Workspace();
+
+        assertEquals("", testWorkspace.viewAllCards());
+    }
+
+    @Test
+    void testOneCard(){
+        Workspace testWorkspace = new Workspace();
+        Card testCard = new Card("title", 0, "description", "Deadline");
+        testWorkspace.addCard(testCard);
+
+        assertEquals(testWorkspace.getCard(0).getTitle(), testWorkspace.viewAllCards());
+    }
+
+    @Test
+    void testMultipleCards(){
+        Workspace testWorkspace = new Workspace();
+        Card testCard = new Card("title", 0, "description", "Deadline");
+        Card testCard2 = new Card("title2", 1, "description2", "Deadline2");
+        testWorkspace.addCard(testCard);
+        testWorkspace.addCard(testCard2);
+
+        assertEquals(testWorkspace.getCard(0).getTitle(), testWorkspace.viewAllCards());
+    }
+
 
 
 }
