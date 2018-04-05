59a951d2e72ba9107fcc542c133c80e27511336b
yemo06 <yemoafo@github.com>
2018-04-05 19:11:34 -0400

    Imp/Tests for multiplecards with same priotiy

--- src/test/java/edu/ithaca/group2/veiwAllTest.java
+++ src/test/java/edu/ithaca/group2/veiwAllTest.java
@@ -41,6 +41,24 @@ class veiwAllTest {
         assertEquals(titleList+titleList2, testWorkspace.viewAllCards());
         System.out.println(testWorkspace.viewAllCards());
     }
+    @Test
+    void testMutipleCardsandPriorityChange(){
+        Workspace tmc =new Workspace();
+        Card testCard = new Card("title",0,"description","deadline");
+        tmc.addCard(testCard);
+        for (int i = 0; i <4 ; i++) {
+            Card testcard2= new Card("title"+i,0);
+            tmc.addCard(testcard2);
+        }
+        String titleList = tmc.getCard("title3").getPriority()+"\t"+tmc.getCard("title3").getTitle()+"\n";
+        String titleList2 = tmc.getCard("title2").getPriority()+"\t"+tmc.getCard("title2").getTitle()+"\n";
+        String titleList3 = tmc.getCard("title1").getPriority()+"\t"+tmc.getCard("title1").getTitle()+"\n";
+        String titleList4 = tmc.getCard("title0").getPriority()+"\t"+tmc.getCard("title0").getTitle()+"\n";
+        String titleList5 = tmc.getCard("title").getPriority()+"\t"+tmc.getCard("title").getTitle();
+        assertEquals(titleList+titleList2+titleList3+titleList4+titleList5,tmc.viewAllCards());
+        System.out.println(tmc.viewAllCards());
+
+    }
 
 
 
